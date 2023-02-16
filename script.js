5cd079bdf23eb9705a707831405bcea49adf7aba
restrepo84 <112986175+restrepo84@users.noreply.github.com>
2023-02-16 13:02:50 -0700

    configuring babel and polyfilling

--- script.js
+++ script.js
@@ -94,8 +94,8 @@ console.log(cart);
 // const { addTocart } = require('./shoppingCart.js')
 
 //* Introduction to NPM *//
-import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
-// import cloneDeep from "lodash-es";
+// import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+import cloneDeep from "lodash-es";
 
 const state = {
   cart: [
@@ -111,3 +111,25 @@ state.user.loggedIn = false;
 console.log(stateClone);
 
 console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  #greeting = "Hey";
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person("Jonas");
+
+console.log("Jonas" ?? null);
+
+console.log(cart.find((el) => el.quantity >= 2));
+Promise.resolve("TEST").then((x) => console.log(x));
+
+// import "core-js/stable";
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
